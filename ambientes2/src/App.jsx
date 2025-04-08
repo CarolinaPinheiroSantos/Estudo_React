16a6068f265df0fdc8143c08aaeb359a0067c2d5
Carolina Pinheiro <pinheirocarol0506@gmail.com>
2025-04-08 15:58:06 -0300

    modelo

---
+++ ambientes2/src/App.jsx
@@ -0,0 +1,12 @@
+import {Login} from './paginas/Login'
+import {Professor} from './paginas/Professor'
+import {Disciplina} from './paginas/Disciplina'
+import {Sala} from './paginas/Sala'
+import {Reserva} from './paginas/Reserva'
+
+export function App() {
+  return (
+    <Reserva />
+  )
+}
+
